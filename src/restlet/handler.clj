9311383a2e45594cf5df5fbfaf80ce875651ddae
y00ser <datais@use.startmail.com>
2016-01-15 21:31:12 +0000

    added capability to download files

--- src/restlet/handler.clj
+++ src/restlet/handler.clj
@@ -1,11 +1,16 @@
 (ns restlet.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.java.io]
+             ))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (compojure.route/files "/public")
+  (GET "/" [] "worked")
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+
